05aa8fa84d9b5c6e9c42c6d0a58af38425592f3c
Mikkel <mikkel.spartan@hotmail.com>
2025-06-12 13:21:10 +0200

    wip

--- petalinux/nfs_setup_rootfs.sh
+++ petalinux/nfs_setup_rootfs.sh
@@ -1,96 +1,96 @@
 #!/bin/bash
-#!/bin/bash
-
-# Skrypt do kopiowania obrazów systemu do katalogów NFS/TFTP
-# Uruchom jako root po zbudowaniu projektu
-
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Ten skrypt musi byæ uruchomiony jako root"
-    exit 1
-fi
-
-# Konfiguracja zmiennych
-NFS_ROOT="/home/NFSshare"
-TFTP_ROOT="/tftpboot"
-BUILD_DIR="images/linux"
-
-# SprawdŸ czy katalogi istniej¹
-if [ ! -d "$BUILD_DIR" ]; then
-    echo "Katalog $BUILD_DIR nie istnieje. Uruchom najpierw petalinux-build."
-    exit 1
-fi
-
-if [ ! -d "$NFS_ROOT" ]; then
-    echo "Katalog NFS $NFS_ROOT nie istnieje. Uruchom najpierw nfs_setup.sh."
-    exit 1
-fi
-
-if [ ! -d "$TFTP_ROOT" ]; then
-    echo "Katalog TFTP $TFTP_ROOT nie istnieje. Uruchom najpierw nfs_setup.sh."
-    exit 1
-fi
-
-# Kopiuj obrazy do katalogów NFS/TFTP
-echo "Kopiowanie obrazów do katalogów NFS/TFTP..."
-
-# ZnajdŸ obraz rootfs
-ROOTFS_IMG=""
-for img in "$BUILD_DIR"/rootfs.tar.gz "$BUILD_DIR"/petalinux-rootfs.tar.gz "$BUILD_DIR"/petalinux-image-minimal-*.tar.gz; do
-    if [ -f "$img" ]; then
-        ROOTFS_IMG="$img"
-        break
-    fi
-done
-
-if [ -z "$ROOTFS_IMG" ]; then
-    echo "Nie znaleziono obrazu rootfs!"
-    exit 1
-fi
-
-echo "Rozpakowanie $ROOTFS_IMG do $NFS_ROOT..."
-rm -rf "$NFS_ROOT"/*  # Czyszczenie katalogu przed rozpakowaniem
-tar -xzf "$ROOTFS_IMG" -C "$NFS_ROOT"
-
-# ZnajdŸ obraz j¹dra
-KERNEL_IMG=""
-for img in "$BUILD_DIR"/Image "$BUILD_DIR"/uImage; do
-    if [ -f "$img" ]; then
-        KERNEL_IMG="$img"
-        break
-    fi
-done
-
-if [ -z "$KERNEL_IMG" ]; then
-    echo "Nie znaleziono obrazu j¹dra!"
-    exit 1
-fi
-
-echo "Kopiowanie $KERNEL_IMG do $TFTP_ROOT/Image..."
-cp "$KERNEL_IMG" "$TFTP_ROOT/Image"
-
-# ZnajdŸ DTB
-DTB_IMG=""
-for img in "$BUILD_DIR"/system.dtb "$BUILD_DIR"/*.dtb; do
-    if [ -f "$img" ] && [ "$img" != "$BUILD_DIR/*.dtb" ]; then
-        DTB_IMG="$img"
-        break
-    fi
-done
-
-if [ -z "$DTB_IMG" ]; then
-    echo "Nie znaleziono pliku DTB!"
-    exit 1
-fi
-
-echo "Kopiowanie $DTB_IMG do $TFTP_ROOT/system.dtb..."
-cp "$DTB_IMG" "$TFTP_ROOT/system.dtb"
-
-# Dodatkowa konfiguracja dla NFS (prawa dostêpu)
-chown -R nobody:nogroup "$NFS_ROOT"
-chmod -R 777 "$NFS_ROOT"
-
-echo "Wszystkie obrazy zosta³y skopiowane."
-echo "Gotowe!"
+##!/bin/bash
+#
+## Skrypt do kopiowania obrazów systemu do katalogów NFS/TFTP
+## Uruchom jako root po zbudowaniu projektu
+#
+#if [ "$(id -u)" -ne 0 ]; then
+#    echo "Ten skrypt musi byæ uruchomiony jako root"
+#    exit 1
+#fi
+#
+## Konfiguracja zmiennych
+#NFS_ROOT="/home/NFSshare"
+#TFTP_ROOT="/tftpboot"
+#BUILD_DIR="images/linux"
+#
+## SprawdŸ czy katalogi istniej¹
+#if [ ! -d "$BUILD_DIR" ]; then
+#    echo "Katalog $BUILD_DIR nie istnieje. Uruchom najpierw petalinux-build."
+#    exit 1
+#fi
+#
+#if [ ! -d "$NFS_ROOT" ]; then
+#    echo "Katalog NFS $NFS_ROOT nie istnieje. Uruchom najpierw nfs_setup.sh."
+#    exit 1
+#fi
+#
+#if [ ! -d "$TFTP_ROOT" ]; then
+#    echo "Katalog TFTP $TFTP_ROOT nie istnieje. Uruchom najpierw nfs_setup.sh."
+#    exit 1
+#fi
+#
+## Kopiuj obrazy do katalogów NFS/TFTP
+#echo "Kopiowanie obrazów do katalogów NFS/TFTP..."
+#
+## ZnajdŸ obraz rootfs
+#ROOTFS_IMG=""
+#for img in "$BUILD_DIR"/rootfs.tar.gz "$BUILD_DIR"/petalinux-rootfs.tar.gz "$BUILD_DIR"/petalinux-image-minimal-*.tar.gz; do
+#    if [ -f "$img" ]; then
+#        ROOTFS_IMG="$img"
+#        break
+#    fi
+#done
+#
+#if [ -z "$ROOTFS_IMG" ]; then
+#    echo "Nie znaleziono obrazu rootfs!"
+#    exit 1
+#fi
+#
+#echo "Rozpakowanie $ROOTFS_IMG do $NFS_ROOT..."
+#rm -rf "$NFS_ROOT"/*  # Czyszczenie katalogu przed rozpakowaniem
+#tar -xzf "$ROOTFS_IMG" -C "$NFS_ROOT"
+#
+## ZnajdŸ obraz j¹dra
+#KERNEL_IMG=""
+#for img in "$BUILD_DIR"/Image "$BUILD_DIR"/uImage; do
+#    if [ -f "$img" ]; then
+#        KERNEL_IMG="$img"
+#        break
+#    fi
+#done
+#
+#if [ -z "$KERNEL_IMG" ]; then
+#    echo "Nie znaleziono obrazu j¹dra!"
+#    exit 1
+#fi
+#
+#echo "Kopiowanie $KERNEL_IMG do $TFTP_ROOT/Image..."
+#cp "$KERNEL_IMG" "$TFTP_ROOT/Image"
+#
+## ZnajdŸ DTB
+#DTB_IMG=""
+#for img in "$BUILD_DIR"/system.dtb "$BUILD_DIR"/*.dtb; do
+#    if [ -f "$img" ] && [ "$img" != "$BUILD_DIR/*.dtb" ]; then
+#        DTB_IMG="$img"
+#        break
+#    fi
+#done
+#
+#if [ -z "$DTB_IMG" ]; then
+#    echo "Nie znaleziono pliku DTB!"
+#    exit 1
+#fi
+#
+#echo "Kopiowanie $DTB_IMG do $TFTP_ROOT/system.dtb..."
+#cp "$DTB_IMG" "$TFTP_ROOT/system.dtb"
+#
+## Dodatkowa konfiguracja dla NFS (prawa dostêpu)
+#chown -R nobody:nogroup "$NFS_ROOT"
+#chmod -R 777 "$NFS_ROOT"
+#
+#echo "Wszystkie obrazy zosta³y skopiowane."
+#echo "Gotowe!"
 # Skrypt do konfiguracji rootfs dla PetaLinux z NFS
 # Uruchom jako root po zbudowaniu projektu
 
